14133f8960c15d43e461e521dc7f75d4c28aecae
Ron Nahmias <ron.nahmias2@gmail.com>
2023-08-24 12:19:16 +0300

    disable header

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,14 +1,7 @@
-import Header from "@/components/header/header.component";
-
 export default async function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      <Header />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
