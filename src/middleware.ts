4dd9d6845e7f1add83198ec7a49546c3472f714f
Ron Nahmias <ron.nahmias2@gmail.com>
2023-08-25 11:55:30 +0300

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: [
-    "/lessons/:path*",
-    "/dashboard",
-    "/lauder-lessons/:path*",
-    "/api/:path*",
-  ],
+  matcher: ["/lessons/:path*", "/dashboard", "/lauder-lessons/:path*"],
 };
